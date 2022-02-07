30bffadc571412b6326cb1fe74a0442e1be93fa2
Marc Byndas <byndasma@gmail.com>
2022-02-07 13:42:09 -0500

    tightening up bubblers colors and speed and size

--- bubblers.js
+++ bubblers.js
@@ -16,18 +16,18 @@ let bubbles = [];
 let bgBubbles = [];
 
 function addBubble() {
-    bubbles.push(new Bubble('rgb(225, 194, 194', 1.8));
+    bubbles.push(new Bubble('#21618C ', 2.5));
 }
 function addBgBubble() {
-    bgBubbles.push(new Bubble('#ad0170', 2.5));
+    bgBubbles.push(new Bubble('#1B4F72', 5));
 }
 class Bubble {
     constructor(color, ySpeed) {
-        this.radius = (Math.random() * 150) + 30;
+        this.radius = (Math.random() * 750);
         this.life = true;
         this.x = (Math.random() * window.innerWidth);
-        this.y = (Math.random() * 0.0000002) + window.innerHeight + this.radius;
-        this.vy = ((Math.random() * 0.0002) + 0.001) + ySpeed;
+        this.y = (Math.random() * 0.0002) + window.innerHeight + this.radius;
+        this.vy = ((Math.random() * 0.0000002)) + ySpeed;
         this.vr = 0;
         this.vx = (Math.random() * 4) - 2;
         this.color = color;
