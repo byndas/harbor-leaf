b599fa21ac87a4338a1e96d2bf3337c6c7b038da
Marc Byndas <byndasma@gmail.com>
2022-04-13 09:32:22 -0400

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const favicon = require('serve-favicon');
-const path = require('path');
-const express = require('express');
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+
 const app = express();
 
-app.use('/css', express.static(process.cwd() + '/css'));
-app.use('/js', express.static(process.cwd() + '/js'));
-app.use(favicon(path.join(__dirname,'favicon','favicon.ico')));
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-app.get('/', function(req, res){
-			res.sendFile(process.cwd() + '/main.html');
-		});
-
-const port = process.env.PORT;
-app.listen(port, function() {
-	console.log('Node.js listening on port ' + port + '...');
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
+
+const port = process.env.PORT || 8080;
+app.listen(port);
